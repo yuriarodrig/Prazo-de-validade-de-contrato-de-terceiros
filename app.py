518b4d22cc623365a807a13a7ea6acb58219727b
yuriarodrig <yurialves3324@gmail.com>
2023-01-25 13:09:59 -0300

    FINAL

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ database = os.getenv('DATABASE_KEY')
 username = os.getenv('USERNAME_DB_KEY')
 password = os.getenv('PASSWORD_DB_KEY')
 #Conexão email
-ACCESS_ID = os.getenv('AWS_ACCESS_ID')
-AWS_REGION_SERVE = os.getenv('AWS_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION_SERVE')
+EMAIL_ID = os.getenv('AWS_ACCESS_ID')
+EMAIL_KEY = os.getenv('AWS_ACCESS_KEY')
+EMAIL_REGIAO = os.getenv('AWS_REGION_SERVE')
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 def emissao_terceiro():
@@ -43,3 +43,42 @@ def emissao_terceiro():
     #Então a diferença da data atual e a data emissão tem que ser 173 dias.   já está perto de 180 dias
     agrupar = (query.loc[(query['Diferença de dias'] == '173 days')])
     #Minha busca de notas está pronta
+    
+    #Se o codigo do fornecedor estiver vazio, se for igual a false vai fazer o envio do email
+    if agrupar['C0D_FORNECEDOR'].empty == False:
+        html_tabela = build_table(agrupar, 'grey_light', font_family='sans-serif')
+        corpo_email = html_tabela
+        #grupo de emails que eu quero enviar essa notificação
+        grupo_email = ['t.gmail.com', 't.gmail.com', 't.gmail.com']
+        titulo = emoji.emojize(':warning: Notas a vencer prazo de 180 dias Poder 3º: ', variant="emoji_type")
+        SENDER = 'WORKFLOW <ENVIAREMAIL@GMAIL.COM>'
+        SUBJECT = titulo
+        BODY_HTML = corpo_email
+        CHARSET = 'UTF-8'
+        
+        #Conexão email 
+        client = boto3.client('ses',region_name=EMAIL_REGIAO,
+                    aws_access_key_id = EMAIL_ID,
+                    aws_secret_access_key= EMAIL_KEY)
+
+        msg = MIMEMultipart('mixed')
+        msg['Subject'] = SUBJECT
+        msg['From'] = SENDER
+        msg['To'] = ', '.join(grupo_email)
+        msg_body = MIMEMultipart('alternative')
+        htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+        msg_body.attach(htmlpart)
+        msg.attach(msg_body)
+        
+        response = client.send_raw_email(
+            Source=SENDER,
+            Destinations = grupo_email,
+            RawMessage={
+                'Data':msg.as_string(),
+            },)
+        print('''email enviado
+        ---------------------''')
+#CLIENTE ESTÁ VAZIO? TRUE(ESTÁ VAZIO)
+    else:
+        print('NÃO TEM NOTA')
+emissao_terceiro()
