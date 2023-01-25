ebad86233e91de2c2021818ab5604c4faa9aa7f4
yuriarodrig <yurialves3324@gmail.com>
2023-01-25 12:47:53 -0300

    Visualização da Query

--- app.py
+++ app.py
@@ -26,3 +26,11 @@ ACCESS_ID = os.getenv('AWS_ACCESS_ID')
 AWS_REGION_SERVE = os.getenv('AWS_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION_SERVE')
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+def emissao_terceiro():
+    cursor = conn.cursor()
+    #Conexão com o banco de dados e buscando a minha query no banco procura a data de emissao, fornecedor, documento e a id do fornecedor
+    query = pd.read_sql(
+        "SELECT B6_EMISSAO AS DATA, B6_DOC AS DOCUMENTO, B6_CLIFOR AS C0D_FORNECEDOR, A2_NOME AS FORNECEDOR FROM SB6010 SB6 INNER JOIN SA2010 SA2 ON B6_CLIFOR = A2_COD WHERE B6_EMISSAO > '20220731'",
+        conn)
+    df = pd.DataFrame(query)
